53f54490a150f7c78c15d67b2ee891a2b1b007f3
zhoutao1161 <zhoutao1161@126.com>
2018-11-26 15:52:12 +0800

    v 2.5.4
    remove redis

--- common/cache.js
+++ common/cache.js
@@ -1,21 +1,20 @@
-var redis  = require('./redis');
 var _      = require('lodash');
 var logger = require('./logger');
 
 var get = function (key, callback) {
     var t = new Date();
-    redis.get(key, function (err, data) {
-        if (err) {
-            return callback(err);
-        }
-        if (!data) {
-            return callback();
-        }
-        data = JSON.parse(data);
-        var duration = (new Date() - t);
-        logger.debug("Cache get "+ key +", value "+ JSON.stringify(data));
-        callback(null, data);
-    });
+    // redis.get(key, function (err, data) {
+    //     if (err) {
+    //         return callback(err);
+    //     }
+    //     if (!data) {
+    //         return callback();
+    //     }
+    //     data = JSON.parse(data);
+    //     var duration = (new Date() - t);
+    //     logger.debug("Cache get "+ key +", value "+ JSON.stringify(data));
+    //     callback(null, data);
+    // });
 };
 
 exports.get = get;
@@ -32,9 +31,9 @@ var set = function (key, value, time, callback) {
     value = JSON.stringify(value);
 
     if (!time) {
-        redis.set(key, value, callback);
+        //redis.set(key, value, callback);
     } else {
-        redis.set(key, time, value, callback);
+        //redis.set(key, time, value, callback);
     }
     var duration = (new Date() - t);
     logger.debug("Cache set "+ key +", value "+ value);
@@ -44,14 +43,14 @@ exports.set = set;
 
 // 获取所有指定key的数量
 var keys = function(keys, callback) {
-    redis.keys(keys, callback)
+    //redis.keys(keys, callback)
 };
 
 exports.keys = keys;
 
 // 删除key
 var del = function(key, callback) {
-    redis.del(key, callback);
+    //redis.del(key, callback);
 }
 
 exports.del = del;
